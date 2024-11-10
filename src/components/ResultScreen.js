1d0a9d9bb7f211f6d29f5f4d7e069c716139d7ec
carllosramon <carloramon100@gmail.com>
2024-11-10 20:45:10 -0300

    botão restart fix

--- src/components/ResultScreen.js
+++ src/components/ResultScreen.js
@@ -3,6 +3,15 @@ import React from 'react';
 import './ResultScreen.css';
 
 function ResultScreen({ result, onRestart }) {
+  const handleRestart = () => {
+    console.log('Botão Reiniciar clicado'); // Verificação de clique
+    if (typeof onRestart === 'function') {
+      onRestart();
+    } else {
+      console.error('onRestart não é uma função');
+    }
+  };
+
   return (
     <div className="result-container">
       <div className="result-box">
@@ -15,7 +24,7 @@ function ResultScreen({ result, onRestart }) {
         ) : (
           <p className="result-details">Infelizmente, você não atende aos critérios de aprovação.</p>
         )}
-        <button className="restart-button" onClick={onRestart}>
+        <button className="restart-button" onClick={handleRestart}>
           Reiniciar Análise
         </button>
       </div>
